036e603eec2a2866e06dfa6cd27d49ddb46fb67e
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-08-06 22:03:41 +0200

    Add class Column, add image src to App.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static PropTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} image={this.props.image} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column columnTitle={'Animals'} />
+          <Column columnTitle={'Plants'} />
+          <Column columnTitle={'Minerals'} />
+        </div>
       </section>
     )
   }
