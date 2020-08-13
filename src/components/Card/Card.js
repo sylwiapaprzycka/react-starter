b4511902d1f2d61fa0ca9a93ca7781858b13a808
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-08-13 20:58:09 +0200

    Add ESLint and husky with bugs

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,11 @@ import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
-Card.propTypes = {
-  title: PropTypes.node,
-};
-
 class Card extends React.Component {
   static PropTypes = {
     title: PropTypes.node,
   }
+  
   render() {
     return (
       <section className={styles.component}>
