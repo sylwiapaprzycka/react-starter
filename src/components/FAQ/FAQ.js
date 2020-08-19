6f8ea8fc6fea1d2110947884e092334a68608e60
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-08-19 15:58:13 +0200

    Add Info and FAQ

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+import propTypes from 'prop-types';
+import { faqContents, listData } from '../../data/dataStore';
+
+const Info = () => (
+  <Container>
+    <Hero titleText={ faqContents.title } image={ listData.image } />
+    <h2>{ faqContents.questionOne }</h2>
+    <p>{ faqContents.answerOne }</p>
+    <h2>{ faqContents.questionTwo }</h2>
+    <p>{ faqContents.answerTwo }</p>
+    <h2>{ faqContents.questionThree }</h2>
+    <p>{ faqContents.answerThree }</p>
+  </Container>
+);
+
+Info.propTypes = {
+  title: propTypes.string,
+  image: propTypes.string,
+};
+
+export default Info;
