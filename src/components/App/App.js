e04fdcca27e1ba0df3d01dccef50a1dbd547cb7d
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-08-19 20:04:18 +0200

    Fix drag and drop and edit list container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
+import FAQ from '../FAQ/FAQ';
+import List from '../List/ListContainer';
 import MainLayout from '../MainLayout/MainLayout';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
+import styles from './App.scss';
+
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
-      </Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={ styles.switchWrapper }
+      >
+        <Route exact path='/' component={ Home } />
+        <Route exact path='/info' component={ Info } />
+        <Route exact path='/faq' component={ FAQ } />
+        <Route exact path="/list/:id" component={ List } />
+      </AnimatedSwitch>
     </MainLayout> 
   </BrowserRouter>
 );
